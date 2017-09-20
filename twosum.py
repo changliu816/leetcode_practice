5a3b13fe6b1129e472242e670a0c61c1c8488547
Chang Liu <changliu816@gmail.com>
2017-09-20 06:20:38 -0400

    Update twosum.py

--- twosum.py
+++ twosum.py
@@ -12,3 +12,9 @@ class Solution(object):
 # enumerate 
 # d={}  d.has_key
 # Goal is to find the index, so use dictionary type, store the number as key, and index as value
+
+# *************************************
+# Data structure 
+# 1) Linklist vs arraylist
+# Good:  dynamic data structure, can store item of different size, grow organically
+# Bad: waste memory for referrence/pointer; must be read from the beginning unlike indexing of array; difficult for reverse
